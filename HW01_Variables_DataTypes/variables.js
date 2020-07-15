a8bca10cdf1cc842a6e2cf9a344710a2afdf39f8
Vitalii <vit.bondarchuk@gmail.com>
2020-07-15 13:44:01 +0300

    second commit

--- HW01_Variables_DataTypes/variables.js
+++ HW01_Variables_DataTypes/variables.js
@@ -1,21 +1,21 @@
 // оголошення та ініціалізація цін на товари 
-const priceApple = 15.678, 
-	  priceMango = 123.965,
-	  priceLemon = 90.2345;	  
+const priceApple = 15.678,
+      priceMango = 123.965,
+      priceLemon = 90.2345;	  
 
 // логіка 
 let maxNumber = Math.max(priceApple, priceMango, priceLemon),
-	minNumber = Math.min(priceApple, priceMango, priceLemon),
-	totalSum = priceApple + priceMango + priceLemon,
-	priceWithoutCopeeks = Math.floor(priceApple) + Math.floor(priceMango) + Math.floor(priceLemon),
-	sumRound100 = Math.round(totalSum / 100) * 100,
-	totalSumFloor = Math.floor(totalSum),
-	isSumFloorEven = (Math.floor(totalSum) % 2) ? false : true,
-	sumRest500 = Math.trunc((500-totalSum) * 100)/100,
-	averageValue = totalSum / 3,
-	discount = Math.trunc(Math.random() * 100),
-	discountSum = Math.round(totalSum * discount) / 100,
-	pureRevenue = Math.round(((totalSum / 2) - discountSum) * 100) / 100;
+    minNumber = Math.min(priceApple, priceMango, priceLemon),
+    totalSum = priceApple + priceMango + priceLemon,
+    priceWithoutCopeeks = Math.floor(priceApple) + Math.floor(priceMango) + Math.floor(priceLemon),
+    sumRound100 = Math.round(totalSum / 100) * 100,
+    totalSumFloor = Math.floor(totalSum),
+    isSumFloorEven = (Math.floor(totalSum) % 2) ? false : true,
+    sumRest500 = Math.trunc((500-totalSum) * 100)/100,
+    averageValue = totalSum / 3,
+    discount = Math.trunc(Math.random() * 100),
+    discountSum = Math.round(totalSum * discount) / 100,
+    pureRevenue = Math.round(((totalSum / 2) - discountSum) * 100) / 100;
 	
 // Advanced Level
 let consoleStr = webStr = "";
